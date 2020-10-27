e64c753b0b2e91e0f58fc8f343a1b12fd0c19ecb
Fabio Vettori <fabio_vettori@hotmail.it>
2020-10-27 17:05:22 +0100

    test selettore next() per l'ultima img

--- sandbox.js
+++ sandbox.js
@@ -1,25 +1,24 @@
 $(document).ready(function(){
     // quando clicco su una delle frecce assegno la classe "active" (visibile) all'immagine successiva/precedente
-        if ($('img.active + img').length) {
-            $('.right').click(function(){
-                var img_active = $('img.active');
-                img_active.removeClass('active');
-                img_active.next('img').addClass('active');
-                console.log('test');
-        } else {
+    $('.right').click(function(){
+        var img_active = $('img.active');
+        img_active.removeClass('active');
 
+        if (img_active.next().length) {
+                img_active.next('img').addClass('active');
+        } else {
+            $('.product-images img:first-of-type').addClass('active');
         }
     })
 
     $('.left').click(function(){
         var img_active = $('img.active');
         img_active.removeClass('active');
-        img_active.prev('img').addClass('active');
+
+        if (img_active.prev().length) {
+                img_active.prev('img').addClass('active');
+        } else {
+            $('.product-images img:last-of-type').addClass('active');
+        }
     })
-
-    // TEST
-    // var test = '';
-    // console.log(test.length);
-
-    // console.log($('img.active + img').length);
 })
