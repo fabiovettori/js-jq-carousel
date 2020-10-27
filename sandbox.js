6f2768421e26a0f7be6df0f66f31af4a94ab19a7
Fabio Vettori <fabio_vettori@hotmail.it>
2020-10-27 17:32:32 +0100

    Bonus end

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,17 @@
 $(document).ready(function(){
-    // quando clicco su una delle frecce assegno la classe "active" (visibile) all'immagine successiva/precedente
     $('.right').click(function(){
         var img_active = $('img.active');
         img_active.removeClass('active');
 
+        var marker_active = $('span.active');
+        marker_active.removeClass('active');
+
         if (img_active.next().length) {
-                img_active.next('img').addClass('active');
+            img_active.next('img').addClass('active');
+            marker_active.next('span').addClass('active');
         } else {
             $('.product-images img:first-of-type').addClass('active');
+            $('.markers span:first-of-type').addClass('active');
         }
     })
 
@@ -15,10 +19,15 @@ $(document).ready(function(){
         var img_active = $('img.active');
         img_active.removeClass('active');
 
+        var marker_active = $('span.active');
+        marker_active.removeClass('active');
+
         if (img_active.prev().length) {
-                img_active.prev('img').addClass('active');
+            img_active.prev('img').addClass('active');
+            marker_active.prev('span').addClass('active');
         } else {
             $('.product-images img:last-of-type').addClass('active');
+            $('.markers span:last-of-type').addClass('active');
         }
     })
 })
