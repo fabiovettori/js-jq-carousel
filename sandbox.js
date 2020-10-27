407363d18f4451b5bffb14a4d890e5f621f71147
Fabio Vettori <fabio_vettori@hotmail.it>
2020-10-27 18:03:46 +0100

    end v.01

--- sandbox.js
+++ sandbox.js
@@ -30,4 +30,20 @@ $(document).ready(function(){
             $('.markers span:last-of-type').addClass('active');
         }
     })
+
+    $('.markers span').click(function(){
+        var img_active = $('img.active');
+        img_active.removeClass('active');
+
+        var marker_active = $('span.active');
+        marker_active.removeClass('active');
+
+        if (img_active.prev().length) {
+            img_active.prev('img').addClass('active');
+            marker_active.prev('span').addClass('active');
+        } else {
+            $('.product-images img:last-of-type').addClass('active');
+            $('.markers span:last-of-type').addClass('active');
+        }
+    })
 })
