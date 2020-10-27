6a56b9ab59d3fca2ccbee9f25b3354b0a3d1e58d
Fabio Vettori <fabio_vettori@hotmail.it>
2020-10-27 16:22:11 +0100

    Loop carousel

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,25 @@
-$(document).ready(function{
+$(document).ready(function(){
+    // quando clicco su una delle frecce assegno la classe "active" (visibile) all'immagine successiva/precedente
+        if ($('img.active + img').length) {
+            $('.right').click(function(){
+                var img_active = $('img.active');
+                img_active.removeClass('active');
+                img_active.next('img').addClass('active');
+                console.log('test');
+        } else {
 
+        }
+    })
 
+    $('.left').click(function(){
+        var img_active = $('img.active');
+        img_active.removeClass('active');
+        img_active.prev('img').addClass('active');
+    })
 
-    
+    // TEST
+    // var test = '';
+    // console.log(test.length);
+
+    // console.log($('img.active + img').length);
 })
